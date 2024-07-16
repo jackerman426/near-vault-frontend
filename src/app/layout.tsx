4e2a820ddc6d2c83712e06580e70abc57930b3ba
marios <marios@fundsup.co>
2024-07-16 15:13:37 +0300

    [ADDED] Styling in navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,24 @@ import * as React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const links = [
+  {
+    label: 'Dashboard',
+    path: '/',
+    svg: 'M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605',
+  },
+  {
+    label: 'Vaults',
+    path: '/vaults',
+    svg: 'M9 16H5a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v1M9 12H4m8 8V9h8v11h-8Zm0 0H9m8-4a1 1 0 1 0-2 0 1 1 0 0 0 2 0Z',
+  },
+  {
+    label: 'Passes',
+    path: '/passes',
+    svg: 'M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm5.178 12.137a4.137 4.137 0 1 1 1.036-8.144A6.113 6.113 0 0 0 8.726 12c0 1.531.56 2.931 1.488 4.006a4.114 4.114 0 0 1-1.036.131ZM10.726 12c0-1.183.496-2.252 1.294-3.006A4.125 4.125 0 0 1 13.315 12a4.126 4.126 0 0 1-1.294 3.006A4.126 4.126 0 0 1 10.726 12Zm4.59 0a6.11 6.11 0 0 1-1.489 4.006 4.137 4.137 0 1 0 0-8.013A6.113 6.113 0 0 1 15.315 12Z',
+  },
+]
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -40,7 +58,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="flex flex-col md:flex-row h-screen bg-gray-50 text-gray-900">
-          <Navigation />
+          <Navigation links={links} />
           {/*{children}*/}
           <div className="flex-grow p-4 md:p-24 overflow-auto text-sm">
             {children}
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,13 +1,25 @@
 //ICONS TAKEN FROM https://flowbite.com/icons/
 
-import React, { MouseEventHandler, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
+import { useRouter, usePathname } from 'next/navigation'
 import NearLogo from '~/svg/near.svg'
 
 import { useStore } from '@/wallets/useStore'
 import { NearVaultsContract } from '@/config'
+import { cn } from '@/app/lib/utils'
 
-export const Navigation = () => {
+interface NavigationProps {
+  links: Array<{
+    label: string
+    path: string
+    svg: string
+  }>
+}
+
+export const Navigation = ({ links }: NavigationProps) => {
+  const pathname = usePathname()
+
   const { signedAccountId, wallet } = useStore()
   const [action, setAction] = useState<(() => void) | undefined>(undefined)
   const [label, setLabel] = useState('Loading...')
@@ -33,77 +45,41 @@ export const Navigation = () => {
       <div className="flex flex-col justify-between flex-1 mt-8">
         <nav className="-mx-3 space-y-6">
           <div className="space-y-3">
-            <Link
-              className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:text-white"
-              href="/"
-              passHref
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-5 h-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605"
-                />
-              </svg>
-              <span className="mx-2 text-sm font-medium">Dashboard</span>
-            </Link>
-
-            <Link
-              className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:text-white"
-              href="/vaults"
-              passHref
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-5 h-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9 16H5a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v1M9 12H4m8 8V9h8v11h-8Zm0 0H9m8-4a1 1 0 1 0-2 0 1 1 0 0 0 2 0Z"
-                />
-              </svg>
-
-              <span className="mx-2 text-sm font-medium">Vaults</span>
-            </Link>
-
-            <Link
-              className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:text-white"
-              href="/passes"
-              passHref
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-5 h-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm5.178 12.137a4.137 4.137 0 1 1 1.036-8.144A6.113 6.113 0 0 0 8.726 12c0 1.531.56 2.931 1.488 4.006a4.114 4.114 0 0 1-1.036.131ZM10.726 12c0-1.183.496-2.252 1.294-3.006A4.125 4.125 0 0 1 13.315 12a4.126 4.126 0 0 1-1.294 3.006A4.126 4.126 0 0 1 10.726 12Zm4.59 0a6.11 6.11 0 0 1-1.489 4.006 4.137 4.137 0 1 0 0-8.013A6.113 6.113 0 0 1 15.315 12Z"
-                />
-              </svg>
-
-              <span className="mx-2 text-sm font-medium">Passes</span>
-            </Link>
-
-            {/* Repeat similar structure for other links */}
+            {links.map((link) => {
+              const isActive = pathname === link.path
+              return (
+                <li key={`${link.label}-${link.path}`} className="list-none">
+                  <Link
+                    className={cn(
+                      'flex items-center px-3 py-2 transition-colors duration-300 transform rounded-lg hover:bg-blue-400',
+                      isActive
+                        ? 'text-black bg-blue-400'
+                        : 'text-gray-600 dark:text-gray-200',
+                    )}
+                    href={link.path}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-5 h-5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d={link.svg}
+                      />
+                    </svg>
+                    <span className="mx-2 text-sm font-medium">
+                      {link.label}
+                    </span>
+                  </Link>
+                </li>
+              )
+            })}
           </div>
-
           <div className="space-y-3">
             <label className="px-3 text-xs text-white uppercase">Wallet</label>
             <a
